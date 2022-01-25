ca7734705e1ff4d57c651b70f0a88d1571d2a2f9
Benjamin Thorpe <benjaminthorpe19@gmail.com>
2022-01-25 07:38:57 +0000

    replace postgresql for heroku database

--- config.py
+++ config.py
@@ -4,8 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.getenv("SECRET_KEY") or "secret-key-here"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-            .replace("postgres://", "postgresql://") or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://") or \
             'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # MAIL_SERVER = os.getenv("MAIL_SERVER")
