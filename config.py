e66c4bde7668399bc925dd4375c4467f86263f92
Benjamin Thorpe <benjaminthorpe19@gmail.com>
2022-01-25 07:35:38 +0000

    replace postgresql name

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.getenv("SECRET_KEY") or "secret-key-here"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+            .replace("postgres://", "postgresql://") or \
             'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # MAIL_SERVER = os.getenv("MAIL_SERVER")
