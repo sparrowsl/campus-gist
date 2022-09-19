a468d7ce9753c64faa83ca289e4380b471b51ccd
benjithorpe <benthorpe.dev@gmail.com>
2022-09-19 16:27:06 +0000

    add tailwindcss

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				brand: '#25316D',
+				'brand-light': '#B7C4CF',
+				'brand-text': '#222831',
+				'brand-blue': '#364F6B'
+			},
+			fontFamily: {
+				roboto: ['Roboto', 'Arial', 'sans-serif'],
+				barlow: ['Barlow', 'Arial', 'sans-serif']
+			}
+		}
+	},
+	plugins: []
+};
