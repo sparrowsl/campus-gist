f71ff9031b62578139e0fbb59f096dac11af5871
benjithorpe <benthorpe.dev@gmail.com>
2022-09-25 14:52:48 +0000

    refactor: clean up the extra python code

--- campus_gist/static/js/main.js
+++
@@ -1,4 +0,0 @@
-const navbarToggle = document.querySelector('.uk-navbar-toggle');
-navbarToggle.addEventListener('click', () => {
-	document.querySelector('nav ul').classList.toggle('uk-flex');
-});
