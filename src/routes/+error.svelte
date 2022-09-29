ebd42dd27b034d89227f5e63284be1c95300e1f1
benjithorpe <benthorpe.dev@gmail.com>
2022-09-29 05:28:45 +0000

    fix: add top level error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<section class="grid h-screen place-content-center gap-3">
+	<h1 class="text-4xl text-brand">{$page.status}: {$page.error.message}</h1>
+	<a href="/" class="mx-auto block min-w-fit text-brand underline"> Home </a>
+</section>
