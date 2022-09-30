6c32766e6fc524e0d14103ff32d0de4713770858
benjithorpe <benthorpe.dev@gmail.com>
2022-09-30 12:05:32 +0000

    fix: fix top level await issue in vercel deployment

--- src/lib/stores/gists.js
+++ src/lib/stores/gists.js
@@ -1,16 +1,9 @@
-import { browser } from '$app/environment';
 import { writable } from 'svelte/store';
 
 export const gists = writable([]);
 
-async function getGists() {
+export async function getGists() {
 	const res = await fetch('/data/gists.json');
-	if (res.ok) return (await res.json()) || [];
+
+	if (res.ok) return await res.json();
 }
-
-// export async function deleteGist(gist) {
-// 	console.log(gist);
-// 	gists.update(() => gists.filter((g) => g.id !== gist.id));
-// }
-
-if (browser) gists.set(await getGists());
--- src/routes/(feed)/gists/+page.svelte
+++ src/routes/(feed)/gists/+page.svelte
@@ -1,9 +1,11 @@
 <script>
 	import Card from '$lib/components/feed/Card.svelte';
-	import { gists } from '$lib/stores/gists.js';
+	import { gists, getGists } from '$lib/stores/gists.js';
 	import { createGistModal } from '$lib/stores/modals.js';
 	import CreateGistModal from '$lib/components/modals/CreateGistModal.svelte';
 	import CreateGist from '$lib/components/feed/CreateGist.svelte';
+
+	$gists = getGists();
 </script>
 
 <article class="container relative mx-auto min-h-screen max-w-6xl p-3">
@@ -12,13 +14,17 @@
 
 	<!-- Display all the gists -->
 	<section class="flex flex-col gap-3">
-		{#each $gists as gist (gist)}
-			<Card {gist} />
-		{:else}
-			<p class="mt-10 animate-pulse text-center italic text-brand-blue">
-				No gist found, create new gist...
-			</p>
-		{/each}
+		{#await $gists}
+			<div class="text-brand-blue text-center italic animate-pulse">Loading gists...</div>
+		{:then $gists}
+			{#each $gists as gist (gist)}
+				<Card {gist} />
+			{:else}
+				<p class="mt-10 animate-pulse text-center italic text-brand-blue">
+					No gist found, create new gist...
+				</p>
+			{/each}
+		{/await}
 	</section>
 </article>
 
