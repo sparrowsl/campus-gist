2144e1ba29fc137382a83304ff2f275713746086
benjithorpe <benthorpe.dev@gmail.com>
2022-10-02 09:02:43 +0000

    refactor: update login form

--- src/routes/(main)/login/+page.svelte
+++ src/routes/(main)/login/+page.svelte
@@ -31,6 +31,7 @@
 <section class="grid min-h-[90vh] place-content-center">
 	<form
 		action=""
+		method="POST"
 		class="rounded-md bg-white p-8 shadow-md md:w-96 md:p-10"
 		on:submit|preventDefault={handleLogin}
 	>
@@ -43,8 +44,9 @@
 				<label for="" class="block text-sm text-gray-500">Email</label>
 				<input
 					type="email"
-					bind:value={email}
 					required
+					name="email"
+					bind:value={email}
 					class="block w-full rounded-md border-gray-200 p-2 text-gray-600"
 					placeholder="john@gmail.com"
 				/>
@@ -54,8 +56,9 @@
 				<label for="" class="block text-sm text-gray-500">Password</label>
 				<input
 					type="password"
-					bind:value={password}
 					required
+					name="password"
+					bind:value={password}
 					class="block w-full rounded-md border-gray-200 p-2 text-gray-600"
 					placeholder="password"
 				/>
@@ -78,7 +81,7 @@
 				{#if isLoggedIn}
 					<Spinner />
 				{:else}
-					Login
+					Log In
 				{/if}
 			</button>
 		</fieldset>
